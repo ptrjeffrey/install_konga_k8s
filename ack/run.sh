1ed55cdc72cb7a1975fe2a43b2b61c8ffca7ef4c
sean.zhang <sean.zhang@digitwin.com>
2023-01-05 12:10:36 +0800

    update

--- ack/run.sh
+++ ack/run.sh
@@ -1,3 +1,5 @@
 kubectl --kubeconfig ~/.kube/config-aliyun-ack-dev-cn apply -f kong.yaml
 kubectl --kubeconfig ~/.kube/config-aliyun-ack-dev-cn -n denengy apply -f kong-pvc.yaml
 kubectl --kubeconfig ~/.kube/config-aliyun-ack-dev-cn -n denengy cp kong.yml kong-6d6874f695-cchkn:/usr/local/share/lua/5.1/kong/plugins/cas
+kubectl --kubeconfig ~/.kube/config-aliyun-ack-dev-cn -n denengy get pods
+kong migrations up
